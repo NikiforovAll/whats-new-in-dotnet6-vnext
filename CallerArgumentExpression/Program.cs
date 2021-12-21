8abfdb8c2f494777d863bd1f11ee841021d0147a
nikiforovall <alexey.nikiforovall@gmail.com>
2021-12-21 15:42:13 +0200

    Add CallerArgumentExpression

---
+++ CallerArgumentExpression/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+var f = new Foo(new Foo(default, 1), 0);
+
+TestFramework.Assert(f is not null);
+
+Console.WriteLine(f);
+// [new] Extended property patterns​
+TestFramework.Assert(f.Child.Value is > 0);
+
+TestFramework.Assert(f.Child.Child is null);
+
+// [new] built-in null guard
+ArgumentNullException.ThrowIfNull​(f.Child.Child);
+
+public record Foo(Foo? Child, int Value);
+
+public class TestFramework
+{
+    public static void Print(bool result, [CallerArgumentExpression("result")] string? expr = default)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine($"({expr}) is {result}");
+        Console.ResetColor();
+    }
+
+    public static void Assert(bool condition, [CallerArgumentExpression("condition")]
+        string? conditionExpression = default)
+    {
+        if (!condition)
+            throw new Exception($"Condition failed: {conditionExpression}");
+
+        Print(condition, conditionExpression);
+    }
+}
---
+++ ParallelForEachAsync/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+await Parallel.ForEachAsync(Generate(), Handle)
+    .WaitAsync(TimeSpan.FromSeconds(2))
+    .ContinueWith(Report);
+
+static async IAsyncEnumerable<int> Generate()
+{
+    while (true)
+    {
+        var delay = Random.Shared.Next(100);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        WriteLine($"Issued {delay}");
+        ResetColor();
+        await Task.Delay(delay);
+        yield return delay;
+    }
+}
+
+static async ValueTask Handle(int i, CancellationToken ct)
+{
+    await Task.Delay(i, ct);
+    Console.ForegroundColor = ConsoleColor.Green;
+    WriteLine($"Handled {i}");
+    ResetColor();
+}
+
+static void Report(Task t) => WriteLine(
+    $"Finished at: {DateTime.Now:T}, task.Status {t.Status}");
