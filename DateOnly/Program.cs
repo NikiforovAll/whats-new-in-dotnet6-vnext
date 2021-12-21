cf7093191ad35b7cbede94749a55a7e199e27ae7
nikiforovall <alexey.nikiforovall@gmail.com>
2021-12-21 19:46:53 +0200

    Add DateOnly

---
+++ DateOnly/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("\nDefault values...\n");
+DateOnly date = DateOnly.MinValue;
+Write(date, nameof(date));
+
+TimeOnly time = TimeOnly.MinValue;
+Write(time, nameof(time));
+
+Console.WriteLine("\nSetup...\n");
+
+TimeOnly startTime = TimeOnly.Parse("11:00 PM");
+var hoursWorked = 2;
+var endTime = startTime.AddHours(hoursWorked);
+
+Write(startTime, nameof(startTime));
+Write(endTime, nameof(endTime));
+Console.WriteLine("\nTesting...\n");
+Write(TimeOnly.Parse("12:00 AM").IsBetween(startTime, endTime));
